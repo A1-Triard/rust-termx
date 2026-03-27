e4f0bca3eb3c368db3f1e43a029ee8702411e4be
Master <internalmike@gmail.com>
2026-03-27 09:12:34 +0300

    rendering (in progress)

--- src/components/view.rs
+++ src/components/view.rs
@@ -4,17 +4,28 @@ use ooecs::Entity;
 pub struct View {
     pub(crate) visual_parent: Option<Entity>,
     pub(crate) real_render_bounds: Rect,
+    pub(crate) ty: u16,
 }
 
+
 impl View {
-    pub fn new() -> Self {
+    pub const NONE: u16 = 0;
+    pub const DECORATOR: u16 = 1;
+    pub const BACKGROUND: u16 = 2;
+
+    pub fn new(ty: u16) -> Self {
         View {
             visual_parent: None,
             real_render_bounds: Rect { tl: Point { x: 0, y: 0 }, size: Vector::null() },
+            ty,
         }
     }
 
     pub fn visual_parent(&self) -> Option<Entity> {
         self.visual_parent
+    }
+
+    pub fn ty(&self) -> u16 {
+        self.ty
     }
 }
--- src/systems/render.rs
+++ src/systems/render.rs
@@ -17,6 +17,8 @@ pub struct Render {
     visual_children_count: fn(entity: Entity, world: &World) -> usize,
     #[virt]
     visual_child: fn(entity: Entity, world: &World, index: usize) -> Entity,
+    #[virt]
+    render: fn(entity: Entity, world: &mut World, rp: &mut RenderPort),
     #[non_virt]
     add_visual_child: fn(parent: Entity, child: Entity, world: &mut World),
     #[non_virt]
@@ -42,10 +44,13 @@ impl Render {
 
     pub fn visual_children_count_impl(this: &Rc<dyn IsRender>, entity: Entity, world: &World) -> usize {
         let render = this.render();
-        if let Some(decorator) = entity.get::<Decorator>(render.decorator, world) {
-            return if decorator.child.is_some() { 1 } else { 0 };
+        match entity.get::<View>(render.view, world).unwrap().ty {
+            View::DECORATOR | View::BACKGROUND => {
+                let decorator = entity.get::<Decorator>(render.decorator, world).unwrap();
+                if decorator.child.is_some() { 1 } else { 0 }
+            },
+            _ => 0,
         }
-        0
     }
 
     pub fn visual_child_impl(
@@ -55,11 +60,25 @@ impl Render {
         index: usize,
     ) -> Entity {
         let render = this.render();
-        if let Some(decorator) = entity.get::<Decorator>(render.decorator, world) {
-            assert_eq!(index, 0);
-            return decorator.child.unwrap();
+        match entity.get::<View>(render.view, world).unwrap().ty {
+            View::DECORATOR | View::BACKGROUND => {
+                let decorator = entity.get::<Decorator>(render.decorator, world).unwrap();
+                assert_eq!(index, 0);
+                decorator.child.unwrap()
+            },
+            _ => panic!(),
         }
-        panic!()
+    }
+
+    pub render_impl(this: &Rc<dyn IsRender>, entity: Entity, world: &mut World, rp: &mut RenderPort) {
+        let render = this.render();
+        match entity.get::<View>(render.view, world).unwrap().ty {
+            View::BACKGROUND => {
+                let background = entity.get::<Background>(render.background, world).unwrap();
+                rp.fill(|rp, p| rp.text(p, background.color, &background.pattern));
+            },
+            _ => { },
+        }
     }
 
     pub fn add_visual_child_impl(this: &Rc<dyn IsRender>, parent: Entity, child: Entity, world: &mut World) {
@@ -83,6 +102,25 @@ impl Render {
 
     pub fn invalidate_render_impl(_this: &Rc<dyn IsRender>, _entity: Entity, _world: &mut World) {
     }
+
+    fn render_entity(this: &Rc<dyn IsRender>, entity: Entity, world: &World, rp: &mut RenderPort) {
+        if rp.invalidated_rect.intersect(rp.bounds).is_empty() {
+            return;
+        }
+        //if view.visibility() != Visibility::Visible { return; }
+        this.render(entity, rp);
+        let base_offset = rp.offset;
+        let base_bounds = rp.bounds;
+        for i in 0 .. this.visual_children_count(entity, world) {
+            let child = this.visual_child(entity, world, i);
+            let view = entity.get::<View>(render.view, world).unwrap();
+            let bounds = child.margin().shrink_rect(child.render_bounds()).offset(base_offset);
+            rp.bounds = bounds.intersect(base_bounds);
+            rp.offset = Vector { x: bounds.l(), y: bounds.t() };
+            Self::render_entity(this, &child, rp);
+        }
+    }
+
 
     pub fn perform_impl(_this: &Rc<dyn IsRender>, _root: Entity, _world: &mut World) {
     }
