a3046a3502b30111bd76f566899aafec5b300786
Master <internalmike@gmail.com>
2026-03-28 22:18:38 +0300

    use XAML in example

--- examples/background/src/main.rs
+++ examples/background/src/main.rs
@@ -1,10 +1,16 @@
+use basic_oop::obj::IsObj;
+use std::rc::Rc;
 use termx_screen_ncurses::{self};
-use termx::components::background::Background;
+use termx::template::Template;
 use termx::termx::{Termx, TermxExt};
+use termx::xaml::{self};
 
 fn main() {
     let mut screen = unsafe { termx_screen_ncurses::init(None, None).unwrap() };
     let termx = Termx::new();
-    let root = Background::new_entity(&termx);
+    let termx_as_obj: Rc<dyn IsObj> = termx.clone();
+    let xaml = include_str!("ui.xaml");
+    let ui: Box<dyn Template> = xaml::from_str(xaml).unwrap();
+    let (root, _) = ui.load_content(&termx_as_obj);
     termx.run(root, screen.as_mut()).unwrap();
 }
