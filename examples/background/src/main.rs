6552f3cd5dd7bee6093eeb67756208074e54189d
Master <internalmike@gmail.com>
2026-03-29 02:16:34 +0300

    example

--- examples/background/src/main.rs
+++ examples/background/src/main.rs
@@ -1,5 +1,3 @@
-use basic_oop::obj::IsObj;
-use std::rc::Rc;
 use termx_screen_ncurses::{self};
 use termx::template::Template;
 use termx::termx::{Termx, TermxExt};
@@ -8,9 +6,8 @@ use termx::xaml::{self};
 fn main() {
     let mut screen = unsafe { termx_screen_ncurses::init(None, None).unwrap() };
     let termx = Termx::new();
-    let termx_as_obj: Rc<dyn IsObj> = termx.clone();
     let xaml = include_str!("ui.xaml");
     let ui: Box<dyn Template> = xaml::from_str(xaml).unwrap();
-    let (root, _) = ui.load_content(&termx_as_obj);
+    let (root, _) = ui.load_content(&termx);
     termx.run(root, screen.as_mut()).unwrap();
 }
