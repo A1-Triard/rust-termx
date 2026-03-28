205b7a1880a5032e8c17b9e6166b7298f2e98dcd
Master <internalmike@gmail.com>
2026-03-28 13:15:05 +0300

    example

--- examples/background/src/main.rs
+++ examples/background/src/main.rs
@@ -1,9 +1,10 @@
 use termx_screen_ncurses::{self};
+use termx::components::background::Background;
 use termx::termx::{Termx, TermxExt};
 
 fn main() {
     let mut screen = unsafe { termx_screen_ncurses::init(None, None).unwrap() };
     let termx = Termx::new();
-    let root = termx.new_background();
+    let root = Background::new_entity(&termx);
     termx.run(root, screen.as_mut()).unwrap();
 }
